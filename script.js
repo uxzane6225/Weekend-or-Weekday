7f69242c0eb398a6a8742d951f13d8405ff402a8
uxzane6225 <132722108+uxzane6225@users.noreply.github.com>
2025-12-14 16:48:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let inputDay;
+let volumeOn = document.getElementById("songVolumeOn");
+let volumeOff = document.getElementById("songVolumeOff");
+
+let submitDay = document.getElementById("submitDay").onclick = function(){
+    inputDay = document.getElementById("inputDay").value;
+    switch(inputDay){
+        case "Monday":
+        case "Tuesday":
+        case "Wednesday":
+        case "Thursday":
+        case "Friday":
+            document.getElementById("outputDay").textContent = `${inputDay} is a Weekday. :(`;
+            break;
+        case "Saturday":
+        case "Sunday":
+            document.getElementById("outputDay").textContent = `${inputDay} is a Weekend. :)`;
+            break;
+        case "":
+            document.getElementById("outputDay").textContent = `That's just a blank! >:(`;
+            break;
+        case "Caturday":
+            document.getElementById("outputDay").textContent = `(=^·.·^=) Meow!`;
+            break;
+        default:
+            document.getElementById("outputDay").textContent = `${inputDay} is not a day! >:(`;
+            break;
+    }
+}
+
+let songVolumneOn = document.getElementById("songVolumeOn").onclick = function(){
+    audioSong = document.getElementById("audioSong").muted = true;
+    if(volumeOn.style.display === "none"){
+        volumeOn.style.display = "inline"
+        volumeOff.style.display = "none"
+    }else{
+        volumeOn.style.display = "none"
+        volumeOff.style.display = "inline"
+    }
+}
+
+let songVolumneOff = document.getElementById("songVolumeOff").onclick = function(){
+    audioSong = document.getElementById("audioSong").muted = false;
+    if(volumeOff.style.display === "none"){
+        volumeOn.style.display = "none"
+        volumeOff.style.display = "inline"
+    }else{
+        volumeOn.style.display = "inline"
+        volumeOff.style.display = "none"
+    }
+}
