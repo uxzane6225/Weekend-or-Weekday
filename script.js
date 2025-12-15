497c3b3d448ce7def0866ec3cd887b3fc8457bee
uxzane6225 <132722108+uxzane6225@users.noreply.github.com>
2025-12-15 19:34:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-let inputDay;
-var audioSong = document.getElementById("audioSong");
+let inputDay = document.getElementById("inputDay");
+let audioSong = document.getElementById("audioSong");
 let volumeOn = document.getElementById("songVolumeOn");
 let volumeOff = document.getElementById("songVolumeOff");
+
+inputDay.addEventListener("keypress", function(event){
+    if (event.key === 'Enter') {
+        inputDay = document.getElementById("inputDay").value;
+        switch(inputDay){
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                document.getElementById("outputDay").textContent = `${inputDay} is a Weekday. :(`;
+                break;
+            case "Saturday":
+            case "Sunday":
+                document.getElementById("outputDay").textContent = `${inputDay} is a Weekend. :)`;
+                break;
+            case "":
+                document.getElementById("outputDay").textContent = `That's just a blank! >:(`;
+                break;
+            case "Caturday":
+                document.getElementById("outputDay").textContent = `(=^·.·^=) Meow!`;
+                break;
+            default:
+                document.getElementById("outputDay").textContent = `${inputDay} isn't a day! >:(`;
+                break;
+        }
+    }
+})
 
 let submitDay = document.getElementById("submitDay").onclick = function(){
     inputDay = document.getElementById("inputDay").value;
